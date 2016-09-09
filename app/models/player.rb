706d6dadfd1093387d844e3c826802c8d9ec86bd
Evans-Munatsa <gagamel.em@gmail.com>
2016-09-09 08:59:40 +0200

    added core functionality

--- app/models/player.rb
+++ app/models/player.rb
@@ -2,6 +2,6 @@ class Player < ActiveRecord::Base
   # include Counter::Cache
   belongs_to :user
   
-  # validates :name,  :presence => true, :uniqueness => true
+  validates :name,  :presence => true, :uniqueness => true
 
 end
